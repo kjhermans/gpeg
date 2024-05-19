38b19e1861d53d7bc89199d2df58684c87bad8c3
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2024-05-19 15:24:54 +0200

    Lots of ncurses based stepping debugger work; not finished by a long shot. This is just storing work. Warnings also need to be extinguished still.

--- src/gen0/lib/engine/gpege_debug_ncurses.c
+++ src/gen0/lib/engine/gpege_debug_ncurses.c
@@ -45,17 +45,112 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 static int ncurses_init = 0;
 
-static
-void gpege_debug_ncurses_drawscreen
-  ()
+static struct
 {
+  unsigned          noinputlines;
+  unsigned          rununtil;
+  int               stepover;
 }
+gpege_dbgncrs_state =
+{
+  8,
+  0
+};
 
 void gpege_debug_ncurses_exit
   ()
 {
   endwin();
   ncurses_init = 0;
+}
+
+static
+char inputbuf[ 80 ];
+
+static
+char* gpege_dbgncrs_input
+  (unsigned y, unsigned x, unsigned w, char* value)
+{
+  unsigned off = 0, crs = 0, len;
+  int c;
+
+  strncpy(inputbuf, value, sizeof(inputbuf));
+  inputbuf[ sizeof(inputbuf)-1 ] = 0;
+  len = strlen(inputbuf);
+
+  while (1) {
+    move(y,x);
+    addch('[');
+    for (unsigned i=0; i < w; i++) {
+      if (off + i < len) {
+        addch(inputbuf[ off + i ]);
+      } else {
+        addch(' ');
+      }
+    }
+    addch(']');
+    move(y, x + 1 + crs);
+    refresh();
+    switch (c = getch()) {
+    case ERR:
+      return NULL;
+    case '\n':
+      return inputbuf;
+    case KEY_LEFT:
+      if (crs > 0) {
+        --crs;
+      } else if (off > 0) {
+        --off;
+      }
+      break;
+    case KEY_RIGHT:
+DO_KEY_RIGHT:
+      if (crs < w && off + crs < len) {
+        ++crs;
+      } else if (off + w < len) {
+        ++off;
+      }
+      break;
+    case KEY_BACKSPACE:
+      if (len && (off + crs)) {
+        if (off + crs < len) {
+          memmove(
+            &(inputbuf[ off+crs ]),
+            &(inputbuf[ off+crs+1 ]),
+            len - (off+crs+1)
+          );
+        }
+        --len;
+        if (crs) {
+          --crs;
+        } else {
+          --off;
+        }
+      }
+      break;
+    default:
+      if (c >= 32 && c < 127) {
+        if (off + crs < len) {
+          if (len < sizeof(inputbuf)-1) {
+            memmove(
+              &(inputbuf[ off+crs+1 ]),
+              &(inputbuf[ off+crs ]),
+              len - (off+crs)
+            );
+            inputbuf[ off+crs ] = c;
+            ++len;
+            goto DO_KEY_RIGHT;
+          }
+        } else if (off + crs == len) {
+          if (len < sizeof(inputbuf)-1) {
+            inputbuf[ len++ ] = c;
+            inputbuf[ len ] = 0;
+            goto DO_KEY_RIGHT;
+          }
+        }
+      }
+    }
+  }
 }
 
 GPEG_ERR_T gpege_debug_ncurses
@@ -68,142 +163,178 @@ GPEG_ERR_T gpege_debug_ncurses
     initscr(); cbreak(); noecho();
     intrflush(stdscr, FALSE);
     keypad(stdscr, TRUE);
-    gpege_debug_ncurses_drawscreen();
     ncurses_init = 1;
   }
 
-  clear();
-  move(0, 56);
-  addstr("GPEG Ncurses Debugger");
-
-  snprintf(myline, sizeof(myline),
-    "Instr#: %.8d, instrptr: %.6u, input offset: %.6u"
-    , ec->ninstructions
-    , ec->bytecode_offset
-    , ec->input_offset
-  );
-  move(1, 0); addstr(myline);
-  if (opcode == OPCODE_FAILURE) {
-    move(2, 0); addstr("FAIL");
-  } else {
-    char* opcodestring = gpeg_opcode2string(opcode);
-
-    switch (opcode) {
-    case OPCODE_ANY:
-    case OPCODE_FAIL:
-    case OPCODE_FAILTWICE:
-    case OPCODE_NOOP:
-    case OPCODE_RET:
-    case OPCODE_TRAP:
-      snprintf(myline, sizeof(myline), "%s", opcodestring);
-      break;
-    case OPCODE_BACKCOMMIT:   /* single argument */
-    case OPCODE_CALL:
-    case OPCODE_PARTIALCOMMIT:
-    case OPCODE_CATCH:
-    case OPCODE_COMMIT:
-    case OPCODE_JUMP:
-    case OPCODE_CHAR:
-    case OPCODE_END:
-    case OPCODE_OPENCAPTURE:
-    case OPCODE_CLOSECAPTURE:
-    case OPCODE_SKIP:
-    case OPCODE_TESTANY:
-    case OPCODE_VAR:
-      {
-        uint32_t param = GET_32BIT_VALUE(
-          gpege->bytecode.data,
-          ec->bytecode_offset + 4
-        );
-        snprintf(myline, sizeof(myline), "%s %u", opcodestring, param);
+  while (1) {
+    clear();
+    move(0, 0);
+    addstr(
+"===================================================== GPEG Ncurses Debugger =="
+    );
+  
+    snprintf(myline, sizeof(myline),
+      "Instr#: %.8d, instrptr: %.6u, input offset: %.6u"
+      , ec->ninstructions
+      , ec->bytecode_offset
+      , ec->input_offset
+    );
+    move(1, 1); addstr(myline);
+    if (opcode == OPCODE_FAILURE) {
+      move(2, 1); addstr("FAIL");
+    } else {
+      char* opcodestring = gpeg_opcode2string(opcode);
+  
+      switch (opcode) {
+      case OPCODE_RET:
+        gpege_dbgncrs_state.stepover = 0;
+      case OPCODE_ANY:
+      case OPCODE_FAIL:
+      case OPCODE_FAILTWICE:
+      case OPCODE_NOOP:
+      case OPCODE_TRAP:
+        snprintf(myline, sizeof(myline), "%s", opcodestring);
+        break;
+      case OPCODE_BACKCOMMIT:   /* single argument */
+      case OPCODE_CALL:
+      case OPCODE_PARTIALCOMMIT:
+      case OPCODE_CATCH:
+      case OPCODE_COMMIT:
+      case OPCODE_JUMP:
+      case OPCODE_CHAR:
+      case OPCODE_END:
+      case OPCODE_OPENCAPTURE:
+      case OPCODE_CLOSECAPTURE:
+      case OPCODE_SKIP:
+      case OPCODE_TESTANY:
+      case OPCODE_VAR:
+        {
+          uint32_t param = GET_32BIT_VALUE(
+            gpege->bytecode.data,
+            ec->bytecode_offset + 4
+          );
+          snprintf(myline, sizeof(myline), "%s %u", opcodestring, param);
+        }
+        break;
+      case OPCODE_CONDJUMP:     /* double argument */
+      case OPCODE_COUNTER:
+      case OPCODE_INTRPCAPTURE:
+      case OPCODE_RANGE:
+      case OPCODE_TESTCHAR:
+        {
+          uint32_t param0 = GET_32BIT_VALUE(
+            gpege->bytecode.data,
+            ec->bytecode_offset + 4
+          );
+          uint32_t param1 = GET_32BIT_VALUE(
+            gpege->bytecode.data,
+            ec->bytecode_offset + 8
+          );
+          snprintf(myline, sizeof(myline), "%s %u %u"
+            , opcodestring, param0, param1
+          );
+        }
+        break;
+      case OPCODE_QUAD:
+      case OPCODE_ENDISOLATE:
+      case OPCODE_ENDREPLACE:
+      case OPCODE_ISOLATE:
+      case OPCODE_BITMASK:
+      case OPCODE_REPLACE:
+      case OPCODE_SET:
+      case OPCODE_SPAN:
+      case OPCODE_TESTQUAD:
+      case OPCODE_TESTSET:
+        myline[ 0 ] = 0;
+        break;
       }
-      break;
-    case OPCODE_CONDJUMP:     /* double argument */
-    case OPCODE_COUNTER:
-    case OPCODE_INTRPCAPTURE:
-    case OPCODE_RANGE:
-    case OPCODE_TESTCHAR:
-      {
-        uint32_t param0 = GET_32BIT_VALUE(
-          gpege->bytecode.data,
-          ec->bytecode_offset + 4
-        );
-        uint32_t param1 = GET_32BIT_VALUE(
-          gpege->bytecode.data,
-          ec->bytecode_offset + 8
-        );
-        snprintf(myline, sizeof(myline), "%s %u %u", opcodestring, param0, param1);
-      }
-      break;
-    case OPCODE_QUAD:
-    case OPCODE_ENDISOLATE:
-    case OPCODE_ENDREPLACE:
-    case OPCODE_ISOLATE:
-    case OPCODE_BITMASK:
-    case OPCODE_REPLACE:
-    case OPCODE_SET:
-    case OPCODE_SPAN:
-    case OPCODE_TESTQUAD:
-    case OPCODE_TESTSET:
-      myline[ 0 ] = 0;
-      break;
+      move(2, 1); addstr(myline);
     }
-    move(2, 0); addstr(myline);
-  }
-
-  {
-    int x = 0, y = 4;
-    move(y, x);
-    for (unsigned i = ec->input_offset; i < ec->input->size; i++) {
-      if (ec->input->data[ i ] == '\n') {
-        x = 0; y++;
-        if (y > 12) {
-          break;
-        }
-        move(y, x);
-      } else if (ec->input->data[ i ] > 31 && ec->input->data[ i ] < 127) {
-        addch(ec->input->data[ i ]);
-        x++;
-        if (x > 80) {
-          x = 0;
-          y++;
-          if (y > 12) {
-            break;
-          }
-          move(y, x);
-        }
-      } else {
-        char myesc[ 8 ];
-        snprintf(myesc, sizeof(myesc), "\\x%.2x", ec->input->data[ i ]);
-        if (x < 76) {
+  
+    move(3, 0);
+    addstr(
+"===================================================================== Input =="
+    );
+  
+    {
+      int x = 0, y = 4;
+      move(y, x);
+      for (unsigned i = ec->input_offset; i < ec->input->size; i++) {
+        if (ec->input->data[ i ] == '\n') {
           x = 0; y++;
-          if (y > 12) {
+          if (y > 4 + gpege_dbgncrs_state.noinputlines) {
             break;
           }
           move(y, x);
-          addstr(myesc);
+        } else if (ec->input->data[ i ] > 31 && ec->input->data[ i ] < 127) {
+          addch(ec->input->data[ i ]);
+          x++;
+          if (x > 80) {
+            x = 0;
+            y++;
+            if (y > 4 + gpege_dbgncrs_state.noinputlines) {
+              break;
+            }
+            move(y, x);
+          }
         } else {
-          addstr(myesc);
+          char myesc[ 8 ];
+          snprintf(myesc, sizeof(myesc), "\\x%.2x", ec->input->data[ i ]);
+          if (x < 76) {
+            x = 0; y++;
+            if (y > 4 + gpege_dbgncrs_state.noinputlines) {
+              break;
+            }
+            move(y, x);
+            addstr(myesc);
+          } else {
+            addstr(myesc);
+          }
+          x += 4;
         }
-        x += 4;
       }
+    }
+  
+    move(24, 1);
+    addstr("<N>ext <Q>uit <R>unto <S>tepover <I>nput toggle");
+    refresh();
+  
+    if (gpege_dbgncrs_state.stepover) {
+      goto OUT;
+    }
+    if (ec->ninstructions < gpege_dbgncrs_state.rununtil) {
+      goto OUT;
+    }
+    int c = getch();
+    switch (c) {
+    case ERR:
+      return GPEG_ERR_EXIT;
+    case 'q':
+      return GPEG_ERR_EXIT;
+    case 'n': case ' ':
+      goto OUT;
+    case 'r':
+      move(23, 1);
+      addstr("Run to instr#:");
+      char* instrstr = gpege_dbgncrs_input(23, 16, 10, "");
+      unsigned instr = strtoul(instrstr, 0, 10);
+      gpege_dbgncrs_state.rununtil = instr;
+      break;
+    case 's':
+      gpege_dbgncrs_state.stepover = 1;
+      goto OUT;
+      break;
+    case 'i':
+      if (gpege_dbgncrs_state.noinputlines == 8) {
+        gpege_dbgncrs_state.noinputlines = 1;
+      } else {
+        gpege_dbgncrs_state.noinputlines = 8;
+      }
+      break;
     }
   }
 
-  move(24, 1);
-  addstr("<N>ext <Q>uit <R>unto <S>tepover");
-  refresh();
-
-  int c = getch();
-  switch (c) {
-  case ERR:
-    return GPEG_ERR_EXIT;
-  case 'q':
-    return GPEG_ERR_EXIT;
-  case 'n':
-    break;
-  }
-
+OUT:
   return GPEG_OK;
 }
 
