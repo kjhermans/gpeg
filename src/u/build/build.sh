226140fcc30330a5abc9386dcad9ea044d3cc535
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2026-04-13 22:28:46 +0200

    Remove all the junk from the u/build dir.

--- src/u/build/build.sh
+++ src/u/build/build.sh
@@ -5,9 +5,17 @@ set -v
 
 ## round 1
 
+rm -f grammar.asm grammar.byc grammar_bytecode.h grammar_slotmap.h
+rm -f assembly.asm assembly.byc assembly_bytecode.h assembly_slotmap.h
+
 ../compiler/refimpl/gpegc -C -i grammar.gpeg -o grammar.asm -M grammar_slotmap.h
 ../assembler/refimpl/gpega -i grammar.asm -o grammar.byc -S grammar.map
 xxd -i grammar.byc grammar_bytecode.h
+
+../compiler/refimpl/gpegc -C -i assembly.gpeg -o assembly.asm \
+  -M assembly_slotmap.h
+../assembler/refimpl/gpega -i assembly.asm -o assembly.byc -S assembly.map
+xxd -i assembly.byc assembly_bytecode.h
 
 rm -rf compiler assembler engine
 mkdir compiler assembler engine
@@ -21,8 +29,14 @@ cp grammar.* compiler/lib/
 cp grammar_*.h compiler/include/gpeg/compiler/
 make -C compiler/
 
+cp assembly.* assembler/lib/
+cp assembly_*.h assembler/include/gpeg/assembler/
+make -C assembler/
+
 ## round 2
 
 rm -f grammar.asm grammar.byc grammar_bytecode.h grammar_slotmap.h
+rm -f assembly.asm assembly.byc assembly_bytecode.h assembly_slotmap.h
 
 ./compiler/main/gpegc -C -i grammar.gpeg -o grammar.asm -M grammar_slotmap.h
+./compiler/main/gpegc -C -i assembly.gpeg -o assembly.asm -M assembly_slotmap.h
