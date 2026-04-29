3b5cd096ac907235d7bb33d7974ac8e0f5770b9b
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2026-04-29 16:45:12 +0200

    This is going to overthrow compilation of many dependent other projects.

--- bin/install.sh
+++ bin/install.sh
@@ -9,21 +9,20 @@ if [ "x$USER" != "xroot" ]; then
 fi
 
 mkdir -p /usr/local/bin
-cp ./src/gen2/main/compiler/gpegc       /usr/local/bin/
-cp ./src/gen2/main/assembler/gpega      /usr/local/bin/
-cp ./src/gen2/main/disassembler/gpegd   /usr/local/bin/
-cp ./src/gen2/main/engine/gpege         /usr/local/bin/
+cp ./src/compiler/main/gpegc       /usr/local/bin/
+cp ./src/assembler/main/gpega      /usr/local/bin/
+#cp ./src/disassembler/main/gpegd   /usr/local/bin/
+cp ./src/engine/main/gpege         /usr/local/bin/
 
 mkdir -p /usr/local/lib/gpeg
-cp ./src/gen2/lib/compiler/libgpegc.a   /usr/local/lib/gpeg/
-cp ./src/gen2/lib/assembler/libgpega.a  /usr/local/lib/gpeg/
-cp ./src/gen2/lib/engine/libgpege.a     /usr/local/lib/gpeg/
+cp ./src/compiler/lib/libgpegc.a   /usr/local/lib/gpeg/
+cp ./src/assembler/lib/libgpega.a  /usr/local/lib/gpeg/
+cp ./src/engine/lib/libgpege.a     /usr/local/lib/gpeg/
 
 ./bin/arjoin /usr/local/lib/gpeg/libgpeg.a \
-  ./src/gen2/lib/gpeg/libgpeg.a \
-  ./src/gen2/lib/compiler/libgpegc.a \
-  ./src/gen2/lib/assembler/libgpega.a \
-  ./src/gen2/lib/engine/libgpege.a 
+  /usr/local/lib/gpeg/libgpegc.a \
+  /usr/local/lib/gpeg/libgpega.a \
+  /usr/local/lib/gpeg/libgpege.a
 
 ln -sf /usr/local/lib/gpeg/libgpegc.a   /usr/local/lib/libgpegc.a
 ln -sf /usr/local/lib/gpeg/libgpega.a   /usr/local/lib/libgpega.a
@@ -31,4 +30,11 @@ ln -sf /usr/local/lib/gpeg/libgpege.a   /usr/local/lib/libgpege.a
 ln -sf /usr/local/lib/gpeg/libgpeg.a    /usr/local/lib/libgpeg.a
 
 mkdir -p /usr/local/include
-cp -rfL ./src/gen2/include/gpeg         /usr/local/include/
+cp -rfL ./src/compiler/include/gpeg     /usr/local/include/
+cp -rfL ./src/assembler/include/gpeg    /usr/local/include/
+cp -rfL ./src/engine/include/gpeg       /usr/local/include/
+
+echo "I installed the following files:"
+find /usr/local/include/gpeg/
+find /usr/local/lib/libgpeg*
+find /usr/local/bin/gpeg*
