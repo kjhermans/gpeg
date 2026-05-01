56bccd6a61816b1dcbe2987fbdc05f4080e99ab4
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2026-05-01 09:50:34 +0200

    Added an uninstall script to the 'make install'. For convenience.

--- bin/install.sh
+++ bin/install.sh
@@ -34,7 +34,15 @@ cp -rfL ./src/compiler/include/gpeg     /usr/local/include/
 cp -rfL ./src/assembler/include/gpeg    /usr/local/include/
 cp -rfL ./src/engine/include/gpeg       /usr/local/include/
 
-echo "I installed the following files:"
-find /usr/local/include/gpeg/
-find /usr/local/lib/libgpeg*
-find /usr/local/bin/gpeg*
+echo "I left an uninstall script in /usr/local/bin/gpeg_uninstall.sh:"
+
+find /usr/local/include/gpeg/ \
+  | sed -e 's/^/rm -f /' > /usr/local/bin/gpeg_uninstall.sh
+find /usr/local/lib/libgpeg* \
+  | sed -e 's/^/rm -f /' >> /usr/local/bin/gpeg_uninstall.sh
+find /usr/local/bin/gpeg* \
+  | sed -e 's/^/rm -f /' >> /usr/local/bin/gpeg_uninstall.sh
+echo "rm -rf /usr/local/include/gpeg/" >> /usr/local/bin/gpeg_uninstall.sh
+
+chmod 775 /usr/local/bin/gpeg_uninstall.sh
+cat /usr/local/bin/gpeg_uninstall.sh
