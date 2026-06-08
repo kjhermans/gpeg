dcc43e59e5b8c10888a0a51baca585dc32075b69
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2026-06-08 16:26:48 +0200

    Coverage test.

--- src/vhdl/simple/test.sh
+++ src/vhdl/simple/test.sh
@@ -11,13 +11,13 @@ TESTDIR=/tmp/gpeg_vhdl_test_$IDENT
 rm -rf $TESTDIR
 mkdir -p $TESTDIR
 
-./gpegc -N -i $GPEG > $GPEG.asm
-./gpega -i $GPEG.asm > $GPEG.byc
-./gpege -c $GPEG.byc -i $INPUT -v > $TESTDIR/test.log 2>&1
+./gpegc -N -i $GPEG > $TESTDIR/$GPEG.asm
+./gpega -i $TESTDIR/$GPEG.asm > $TESTDIR/$GPEG.byc
+./gpege -c $TESTDIR/$GPEG.byc -i $INPUT -v > $TESTDIR/test.log 2>&1
 
-cat $GPEG.byc | perl -e 'my $c=0; while (1) { my $buf=""; sysread(STDIN, $buf, 4); last if (length($buf) != 4); my $hex = join "", map { sprintf("%02X", ord($_)) } split(//, $buf); print "    bcode_mem($c) <= x\"$hex\";\n"; $c++; } print "    bytecode_size <= to_unsigned(".($c*4).", 32);\n";' > $TESTDIR/bytecode.vhdl
+cat $TESTDIR/$GPEG.byc | perl -e 'my $c=0; while (1) { my $buf=""; sysread(STDIN, $buf, 4); last if (length($buf) != 4); my $hex = join "", map { sprintf("%02X", ord($_)) } split(//, $buf); print "    bcode_mem($c) <= x\"$hex\";\n"; $c++; } print "    bytecode_size <= to_unsigned(".($c*4).", 20);\n";' > $TESTDIR/bytecode.vhdl
 
-cat $INPUT | perl -e 'my $c=0; while (1) { my $buf=""; sysread(STDIN, $buf, 1); last if (!length($buf)); my $hex = sprintf("%.2x", ord($buf)); print "    inp_mem($c) <= x\"$hex\";\n"; ++$c; } print "    input_size_s <= to_unsigned($c, 32);\n";' > $TESTDIR/input.vhdl
+cat $INPUT | perl -e 'my $c=0; while (1) { my $buf=""; sysread(STDIN, $buf, 1); last if (!length($buf)); my $hex = sprintf("%.2x", ord($buf)); print "    inp_mem($c) <= x\"$hex\";\n"; ++$c; } print "    input_size_s <= to_unsigned($c, 20);\n";' > $TESTDIR/input.vhdl
 
 sed "/--BYTECODE--/e cat $TESTDIR/bytecode.vhdl" \
   < gpeg_engine_tb_template.vhdl \
@@ -33,5 +33,5 @@ cd $TESTDIR
 
 ghdl -a --std=08 gpeg_engine.vhdl gpeg_engine_tb.vhdl && \
 ghdl -e --std=08 gpeg_engine_tb && \
-ghdl -r --std=08 gpeg_engine_tb
+ghdl -r --std=08 gpeg_engine_tb >> $TESTDIR/test.log 2>&1
 
--- src/vhdl/simple/tests.sh
+++ src/vhdl/simple/tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ./test.sh coverage coverage.gpeg coverage.txt
+./test.sh json json.gpeg json.txt
